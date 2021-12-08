4bd3c774ed0c3f9d93280137b66eb5b715769221
S. S <44981224+odros@users.noreply.github.com>
2021-12-07 21:51:03 -0600

    Add files via upload

--- retrieve.py
+++ retrieve.py
@@ -1,30 +1,83 @@
 ## Retrieve XML for given date
-from ftplib import FTP
 
-# Next iteration: turn into a function that takes the date in YYYYMMDD format
+## In the next versions
+# Allow retrieval of multiple dates at once
+# Give the user some feedback on downloads: while and when done
+# Allow user to specify directory, with current cworking directory as default
 
-# Connect
-ftp = FTP("ted.europa.eu")
-ftp.login("guest", "guest")
 
-# Parse date
-date = "20120516"
-year = date[0:4]
-month = date[4:6]
-day = date[6:8]
-
-# Retrieve directory contents for given year/month combination
-ftp.cwd("daily-packages/" + year + "/" + month)
-# Couldn't find a way to do this without Pandas, should be possible and more efficient
-file_names = ftp.nlst()
-
-# Determine filename (should make robust if it doesn't exist)
-file_name = [file_name for file_name in file_names if file_name.startswith(date)][0]
-
-# Retrieve file
-ftp.retrbinary("RETR " + file_name, open(file_name, 'wb').write)
-# Possible improvement for next iteration: add some feedback on status of download.
-
-# Close connection
-ftp.quit()
-
+def retrieve(date):
+   
+   # Cast date to string
+   date = str(date)
+   
+   ## Determine if requested date is within valid range
+   
+   # Import required library
+   from datetime import datetime
+   
+   # Define range of valid dates
+   start = datetime.strptime("20110101", "%Y%m%d")
+   end = datetime.now()
+   
+   # Parse date into 'datetime' format
+   parsed_date = datetime.strptime(date, "%Y%m%d")
+   
+   # Break if date is out of range
+   if not start <= parsed_date <= end:
+       print("Requested date is out of range.")
+   else:     
+      try:
+         # Parse year and month
+         year = date[0:4]
+         month = date[4:6]
+           
+         # Connect to server
+         from ftplib import FTP
+         ftp = FTP("ted.europa.eu")
+         ftp.login("guest", "guest")
+      
+         # Navigate to directory containing passed date
+         ftp.cwd("daily-packages/" + year + "/" + month)
+         
+         # Retrieve directory contents for given year/month combination  
+         file_names = ftp.nlst()
+         
+         # Determine if a file for the given date exists in directory
+         if any(date in s for s in file_names):
+            
+            # Determine filename using a list comprehension
+            file_name = [file_name for file_name in file_names if file_name.startswith(date)][0]
+            
+            # Retrieve file
+            ftp.retrbinary("RETR " + file_name, open(file_name, 'wb').write)
+            # Possible improvement for next iteration: add some feedback on status of download.
+            
+            # Alert user of successful download
+            print("File was downloaded successfully.")
+            
+            # Close connection
+            ftp.quit()
+         
+         else:
+            print("No file exists for the requested date.")
+            ftp.quit()
+      
+      # Catch file transfer issues
+      except:
+         print("Something went wrong with the file transfer.")
+      
+# Some dates to try
+# 1: should be out of range
+retrieve(1)
+retrieve("A")
+# 20000101: should be able to manage either srings or numbers
+retrieve(20000101)
+# "20000101": should be out of range
+retrieve("20000101")
+# "20170101"
+retrieve("20170101")
+# "20170102"
+retrieve("20170102")
+# "20170103"
+retrieve("20211207")
